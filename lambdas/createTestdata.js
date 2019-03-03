e0c10128d4c23df3c0211d6e830922e166d43f4b
Måns Nilsson <mans.woxulv@gmail.com>
2019-03-03 20:57:43 +0100

    Await promises in array

--- lambdas/createTestdata.js
+++ lambdas/createTestdata.js
@@ -8,35 +8,37 @@ export async function main(event, context) {
     ? (data = JSON.parse(event.body))
     : (data = event.body)
 
-  data.Items.map(async item => {
-    console.log('EVENT BELOW')
-    console.log(item)
-    const params = await {
-      TableName: data.TableName,
-      Item: {
-        Team: item.Item.Team,
-        BlockUuid: `${item.Item.Block}_` + uuid.v1(),
-        Team: item.Item.Team,
-        Block: item.Item.Block,
-        BlockDescription: item.Item.BlockDescription,
-        ItemHeader: item.Item.ItemHeader,
-        ItemText: item.Item.ItemText,
-        CreatedBy: event.requestContext.identity.cognitoIdentityId,
-        CreatedAt: Date.now(),
-        LastUpdatedBy: event.requestContext.identity.cognitoIdentityId,
-      },
-    }
-    console.log('PARAMS BELOW')
-    console.log(params)
-    try {
-      console.log('TRY')
-      await dynamoDbLib.call('put', params)
-      console.log('AFTER dynamoDbLib.call') // DOES FIRE! ASYNC/AWAIT PROBLEM?
-      return await success(params.Item)
-    } catch (e) {
-      console.log('CATCH')
-      console.log(e)
-      return failure({ status: false })
-    }
-  })
+  await Promise.all(
+    data.Items.map(async item => {
+      console.log('EVENT BELOW')
+      console.log(item)
+      const params = {
+        TableName: data.TableName,
+        Item: {
+          Team: item.Item.Team,
+          BlockUuid: `${item.Item.Block}_` + uuid.v1(),
+          Team: item.Item.Team,
+          Block: item.Item.Block,
+          BlockDescription: item.Item.BlockDescription,
+          ItemHeader: item.Item.ItemHeader,
+          ItemText: item.Item.ItemText,
+          CreatedBy: event.requestContext.identity.cognitoIdentityId,
+          CreatedAt: Date.now(),
+          LastUpdatedBy: event.requestContext.identity.cognitoIdentityId,
+        },
+      }
+      console.log('PARAMS BELOW')
+      console.log(params)
+      try {
+        console.log('TRY')
+        await dynamoDbLib.call('put', params)
+        console.log('AFTER dynamoDbLib.call') // DOES FIRE! ASYNC/AWAIT PROBLEM?
+        return success(params.Item)
+      } catch (e) {
+        console.log('CATCH')
+        console.log(e)
+        return failure({ status: false })
+      }
+    })
+  )
 }

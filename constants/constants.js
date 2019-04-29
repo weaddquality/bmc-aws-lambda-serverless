40d67b04cc249fc05ac1d0c3fc819155fbabbc1e
Stefan Franzén <stefan.franzn@gmail.com>
2019-04-29 08:48:20 +0200

    Refactor/list (#7)
    * Add constants for block names
    
    * Use constants in list lambda, and change to use block names as properties instead of an array of blocks with items
    
    * Remove temporary console log
    
    * Add kebabCase to response
    
    * Revert list lambda back to the lastest master revision
    
    * Add temporary listv2 lambda with refactored code
    
    * Rename: kebabCase --> blockInKebabCase
    
    * Add and use BLOCKNAMES export

---
+++ constants/constants.js
@@ -0,0 +1,20 @@
+export const KEY_PARTNERS = 'Key Partners'
+export const KEY_ACTIVITIES = 'Key Activities'
+export const KEY_RESOURCES = 'Key Resources'
+export const VALUE_PROPOSITIONS = 'Value Propositions'
+export const CUSTOMER_RELATIONSHIPS = 'Customer Relationships'
+export const CHANNELS = 'Channels'
+export const CUSTOMERS_SEGMENTS = 'Customer Segments'
+export const COST_STRUCTURES = 'Cost Structures'
+export const REVENUE_STREAMS = 'Revenue Streams'
+export const BLOCKNAMES = [
+  KEY_PARTNERS,
+  KEY_ACTIVITIES,
+  KEY_RESOURCES,
+  VALUE_PROPOSITIONS,
+  CUSTOMER_RELATIONSHIPS,
+  CHANNELS,
+  CUSTOMERS_SEGMENTS,
+  COST_STRUCTURES,
+  REVENUE_STREAMS,
+]
